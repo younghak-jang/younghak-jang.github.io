95dcc7bf702ed9a746e38310346d3b5ed52c6e3b
Marguerite Oneto <marguerite@oneto.us>
2015-12-01 06:59:00 -0700

    Uploaded biz_main_IndContractChart.js by MO

---
+++ biz_main_IndContractChart.js
@@ -0,0 +1,49 @@
+
+// refresh data based on UI values
+function refreshData(commodity) {
+  var file = fileName[commodity];
+  if (file == null) {
+      alert('Raw data not found for ' + commodity);
+      return;
+  }
+
+  // load csv and filter
+  if (data[commodity] == null) {
+	$("body").css("cursor", "progress");
+    console.log(': data not found for ' + commodity + ', start loading ...');
+    try {
+    d3.text('data/' + file, function(error, text) {
+      if (error) {
+		    $("body").css("cursor", "default");
+		    throw error;
+		    console.log('error in d3.text read ...');
+	  };
+      var temp = d3.csv.parse(header + '\n' + text);
+      console.log('temp file created for ' + commodity + ' with ' + temp.length + ' rows.');
+      // cache data
+      data[commodity] = temp;
+      console.log(': data[commodity] load completed for ' + commodity + ' with ' + data[commodity].length + ' rows.');
+	  $("body").css("cursor", "default");
+	  
+	  refreshChart(data[commodity]);
+    })
+    }
+    catch(err) {
+    console.log('try failed');
+    };
+  }
+  else {
+     console.log('data[commodity] already exists.  in else statement.');
+     refreshChart(data[commodity]);
+  };
+}
+
+// refresh chart for the new context
+function refreshChart(csv_data) {
+  if (data[commodity].length == 0) {
+    return;
+  };
+
+  plot_IndContractChart(csv_data);
+
+}
